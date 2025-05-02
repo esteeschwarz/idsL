afa97cdd5fa55fbe2fa29bce138176cd2cc9098a
esteeschwarz <91368979+esteeschwarz@users.noreply.github.com>
2025-05-02 08:12:14 +0200

    upd fetch git

--- backend/R/api/getversion.R
+++ backend/R/api/getversion.R
@@ -1,6 +1,7 @@
 library(jsonlite)
 getversion<-function(){
-vdf<-read.csv(paste0(Sys.getenv("GIT_TOP"),"/idsL/docs/versiontable.csv"))
+vdf<-read.csv(paste0(Sys.getenv("GIT_RAW"),"/idsL/main/docs/versiontable.csv"))
+#vdf<-read.csv(paste0(Sys.getenv("GIT_TOP"),"/idsL/docs/versiontable.csv"))
 lv<-vdf[length(vdf$latestVersion),]
 vjs<-toJSON(lv)
 print(vjs)
