f173dcbde695ec8b3fbddf1687c6e7fcdc0833ec
estee <91368979+esteeschwarz@users.noreply.github.com>
2025-05-04 14:33:46 +0200

    api send build as string

--- backend/R/api/getversion.R
+++ backend/R/api/getversion.R
@@ -3,10 +3,15 @@ getversion<-function(){
 vdf<-read.csv(paste0(Sys.getenv("GIT_RAW"),"/idsL/main/docs/versiontable.csv"))
 #vdf<-read.csv(paste0(Sys.getenv("GIT_TOP"),"/idsL/docs/versiontable.csv"))
 lv<-vdf[length(vdf$latestVersion),]
+ mode(lv[1,])<-"character"
+print(lv)
+
 vjs<-toJSON(lv)
 print(vjs)
 # return(list(vjs))
-vjs
+# vjs
 return(as.list(lv))
 }
               
+
+# getversion()
